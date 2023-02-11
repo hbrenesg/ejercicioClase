5745b1aae9dd9eac3b8e15c674cc48c1c27f62a2
hbrenesg <hbrenesg@ucenfotec.ac.cr>
2023-02-11 11:41:12 -0600

    Ejercicio de la clase 3, parte inicial
    Esta es la parte base de la clase #3

---
+++ clase3EjemploCodigo/resources/js/logica.js
@@ -0,0 +1,35 @@
+// declaración: guarda o reserva un pequeño espacio en la memoria temporal
+
+// variable: espacio de memoria que puede cambiar su contenido a lo largo de la ejecución
+// indica una variable global
+var title;
+
+// indicacion de variable estructural
+let subtitle;
+
+// constante es un espacio de memoria que una vez asignado no va a cambiar su valor
+const height = 200;
+
+// inicializar es dar valor a un espacio reservado de memoria
+title = 'Este es mi primer ejercicio de JS'
+subtitle = 'que emoción'
+
+// JS es un lenguaje no tipado
+// Diferentes tipos de datos 
+// string = texto
+// number = números
+// array = listas 
+// boolean = verdadero do falso
+
+// array
+List = ['manzana', 'pera', 'sandia']
+
+// Objeto es la represetanción de un elemento del mundo real
+var user = {
+    name: 'Hermes',
+    lastname: 'Brenes',
+    age: 28
+}
+// JSON
+
+
